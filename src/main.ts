4f09a9e04dd1dc317bbd07a6733c7387bf49cfe9
lbessen <l.bessen@gmx.de>
2026-04-23 22:19:52 +0200

    Scripts aktualisiert

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,33 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-import "./scripts/npc_begruesser";
-import "./scripts/test";
-
-let currentPopup: any = undefined;
-let inZone = false;
+import "./scripts/begruesser";
 
 WA.onInit().then(() => {
 
     console.log("Scripting API ready");
 
-    // Extra API starten
     bootstrapExtra().then(() => {
         console.log("Scripting API Extra ready");
     }).catch(e => console.error(e));
 
-    // ===== Pflanze Zone =====
-
+    // Pflanze Zone
     WA.room.area.onEnter("pflanze_zone").subscribe(() => {
-        inZone = true;
-        WA.ui.displayActionMessage({
-            message: "Drücke X zum Interagieren"
+        WA.ui.actionBar.addButton({
+            id: "pflanze-button",
+            label: "Interagieren",
+            callback: () => {
+                WA.ui.openPopup(
+                    "pflanzePopup",
+                    "Hier wächst eine schöne Pflanze 🌿",
+                    [{ label: "OK", callback: (popup) => popup.close() }]
+                );
+            }
         });
     });
 
     WA.room.area.onLeave("pflanze_zone").subscribe(() => {
-        inZone = false;
-        WA.ui.removeActionMessage();
-    });
-
-    WA.onKeyPress("x", () => {
-        if (!inZone) return;
-
-        currentPopup = WA.ui.openPopup(
-            "pflanzePopup",
-            "Hier wächst eine schöne Pflanze 🌿",
-            [{
-                label: "OK",
-                callback: (popup) => popup.close()
-            }]
-        );
+        WA.ui.actionBar.removeButton("pflanze-button");
     });
 
 }).catch(e => console.error(e));
--- src/scripts/interaktion.ts
+++ src/scripts/interaktion.ts
@@ -1,32 +1,25 @@
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+/// <reference types="@workadventure/iframe-api-typings" />
 
-bootstrapExtra().then(() => {
+WA.onInit().then(() => {
 
-  let inZone = false;
-
-  WA.room.area.onEnter("pflanze_zone", () => {
-    inZone = true;
-    WA.ui.displayActionMessage({
-      message: "Drücke E um zu interagieren"
+    WA.room.area.onEnter("pflanze_zone").subscribe(() => {
+        WA.ui.actionBar.addButton({
+            id: "pflanze-button",
+            label: "Interagieren",
+            callback: () => {
+                WA.ui.openPopup(
+                    "pflanzePopup",
+                    "Hier wächst eine schöne Pflanze 🌿",
+                    [{ label: "OK", callback: (popup) => popup.close() }]
+                );
+            }
+        });
     });
-  });
 
-  WA.room.area.onLeave("pflanze_zone", () => {
-    inZone = false;
-    WA.ui.removeActionMessage();
-  });
+    WA.room.area.onLeave("pflanze_zone").subscribe(() => {
+        WA.ui.actionBar.removeButton("pflanze-button");
+    });
 
-  WA.onKeyPress("e", () => {
-    if (!inZone) return;
+}).catch(e => console.error(e));
 
-    WA.ui.openPopup(
-      "pflanzePopup",
-      "Hier wächst eine schöne Pflanze 🌿",
-      [{
-        label: "OK",
-        callback: (popup) => popup.close()
-      }]
-    );
-  });
-
-});
+export {};
